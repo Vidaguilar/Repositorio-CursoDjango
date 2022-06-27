88c638c59d37c9f24d141e5c493a108a5d623a00
Vidaguilar <vidaguilar22102001@gmail.com>
2022-06-27 18:11:53 -0500

    agregada Confifuracion admin

--- CursosDjango/Cursos/models.py
+++ CursosDjango/Cursos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+class Cursos(models.Model): 
+    Materia = models.CharField(max_length=12,verbose_name="Mat") 
+    Grado = models.TextField()
+    Descripcion = models.TextField() 
+    carrera = models.TextField()
+    Profesor = models.TextField()
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+        ordering = ["created"] 
+    def __str__(self):
+      return self.Materia
